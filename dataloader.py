3ab79506922163b243650fb659e94244069592b7
Wes Brewer <brewerwh@ornl.gov>
2023-07-24 22:15:12 -0400

    Get dataloader.py working for multiple timesteps and sequences

--- dataloader.py
+++ dataloader.py
@@ -11,7 +11,7 @@ class DataLoader():
         self.path = path
         self.verbose = verbose
 
-    def read_solution(self, time: str):
+    def load_single_timestep(self, time: str):
         # Read solution values from OpenFOAM simulation
         stime = str(time)
         p = readscalar(self.path, stime, 'p.gz')
@@ -53,30 +53,76 @@ class DataLoader():
 
         return X, Y
 
+    def load_multiple_timesteps(self, write_interval, num_timesteps):
+
+        p = readscalar(self.path, str(write_interval), 'p.gz')
+        num_pts = p.shape[0]
+        
+        print(num_pts, num_timesteps)
+
+        p = np.zeros((num_timesteps, num_pts))
+        u = np.zeros((num_timesteps, num_pts))
+        v = np.zeros((num_timesteps, num_pts))
+        wz = np.zeros((num_timesteps, num_pts))
+
+        for i, ts in enumerate(range(write_interval, write_interval*num_timesteps+1, write_interval)):
+            print(i, ts)
+            p[i, :] = readscalar(self.path, str(ts), 'p.gz')
+            u[i, :], v[i, :], _ = readvector(self.path, str(ts), 'U.gz')
+            _, _, wz[i, :] = readvector(self.path, str(ts), 'vorticity.gz')
+
+        X, Y = np.stack((p, u, v), axis=-1), wz
+        return X, Y
+
     def to_csv(self, Y, X, time, columns):
         """Output CSV file named by timestamp, e.g. 1000.csv"""
         df = pd.DataFrame(np.concatenate((Y, X), axis=1), columns=columns)
         df.to_csv(str(time) + '.csv', index=False)
 
-    def create_sequences(path, sequence_length):
-        """Read the CSV files and create sequences"""
-        files = sorted([f for f in os.listdir(path) if f.endswith('.csv')])
-        sequences = []
-        labels = []
+def create_sequences_from_csv(path, sequence_length):
+    """Read the CSV files and create sequences"""
+    files = sorted([f for f in os.listdir(path) if f.endswith('.csv')])
+    sequences = []
+    labels = []
 
-        for i in range(sequence_length, len(files) + 1):
-            sequence = []
-            label_seq = []
-            for j in range(i-sequence_length, i):
-                file = files[j]
-                df = pd.read_csv(os.path.join(path, file))
-                label_seq.append(df.iloc[:, 0].values)  # assume the first column is the target
-                sequence.append(df.iloc[:, 1:].values)  # rest of the columns are features
+    for i in range(sequence_length, len(files) + 1):
+        sequence = []
+        label_seq = []
+        for j in range(i-sequence_length, i):
+            file = files[j]
+            df = pd.read_csv(os.path.join(path, file))
+            label_seq.append(df.iloc[:, 0].values)  # assume the first column is the target
+            sequence.append(df.iloc[:, 1:].values)  # rest of the columns are features
 
-            sequences.append(np.array(sequence))
-            labels.append(np.array(label_seq))
+        sequences.append(np.array(sequence))
+        labels.append(np.array(label_seq))
 
-        return np.array(sequences), np.array(labels)
+    return np.array(sequences), np.array(labels)
+
+def create_sequences(X, Y, window_size=3):
+    """
+    Create sequences of a given window size from the input arrays X and Y.
+
+    Parameters:
+    X (numpy.ndarray): Input array X with shape (nt, nsamples, nvars).
+    Y (numpy.ndarray): Input array Y with shape (nt, nsamples).
+    window_size (int): Size of the sliding window for creating sequences.
+
+    Returns:
+    numpy.ndarray: Array of sequences for X with shape (nt - window_size + 1, nsamples, window_size, nvars).
+    numpy.ndarray: Array of sequences for Y with shape (nt - window_size + 1, nsamples, window_size).
+    """
+    nt, nsamples, nvars = X.shape
+    num_sequences = nt - window_size + 1
+
+    X_sequences = np.zeros((num_sequences, nsamples, window_size, nvars))
+    Y_sequences = np.zeros((num_sequences, nsamples, window_size))
+
+    for i in range(num_sequences):
+        X_sequences[i] = X[i:i+window_size].transpose(1, 0, 2)
+        Y_sequences[i] = Y[i:i+window_size].transpose(1, 0)
+
+    return (X_sequences, Y_sequences)
 
 
 if __name__ == "__main__":
@@ -85,11 +131,15 @@ if __name__ == "__main__":
     
     dl = DataLoader(args.path)
     
-    X, Y = dl.read_solution('1000')
-    print(X.shape, Y.shape)
+    #X, Y = dl.read_solution('1000')
+    #print(X.shape, Y.shape)
 
-    # Read solution values from OpenFOAM simulation
-    for i, ts in enumerate(range(100, 10100, 100)):
-        print(i, ts)
-        X, Y = dl.read_solution(ts)
-        dl.to_csv(Y, X, ts, columns=['wz', 'p', 'Ux', 'Uy'])
+#    # Read solution values from OpenFOAM simulation
+#    for i, ts in enumerate(range(100, 10100, 100)):
+#        print(i, ts)
+#        X, Y = dl.load_single_timestep(ts)
+#        dl.to_csv(Y, X, ts, columns=['wz', 'p', 'Ux', 'Uy'])
+
+    X, Y = create_sequences(*dl.load_multiple_timesteps(100, 100))
+    
+    print(X.shape, Y.shape)
